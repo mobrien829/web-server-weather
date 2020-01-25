735f900043c2092b0da59550536b9d5c4a339195
Michael <mobrien@colgate.edu>
2020-01-25 00:43:39 -0500

    set up basic routes in express

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 const express = require("express");
 
 const app = express();
+
+// first argument is the route, second is what is provided on the route
+// app(route, (request, response) => {})
+app.get("", (req, res) => {
+  res.send("Hello express!");
+});
+
+app.get("/help", (req, res) => {
+  res.send("This is the help page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Learn about us!");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("this is the weather page");
+});
+
+// .listen sets up server (port, callback)
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
