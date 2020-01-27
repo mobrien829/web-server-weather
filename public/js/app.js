efe8e9c13777b5fc8b2814f5804cc9937a293588
Michael <mobrien@colgate.edu>
2020-01-27 00:36:25 -0500

    set up basic fetch request from frontend for weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 console.log("client side JS is loaded");
+
+const searchForm = document.getElementById("search-form");
+const formInput = document.getElementById("search-value");
+const errorMessage = document.getElementById("error-response");
+const forecastMessage = document.getElementById("forecast-response");
+
+searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  handleFetch(formInput.value);
+});
+
+const handleFetch = location => {
+  fetch(`http://localhost:3000/weather?location=${location}`)
+    .then(res => res.json())
+    .then(data => console.log(data.forecast, data.location));
+};
