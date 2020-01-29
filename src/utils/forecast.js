04fabe529de281294458c462043f1faa57dec3b7
Michael <mobrien@colgate.edu>
2020-01-29 11:50:06 -0500

    changed a line for precipitation in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,9 +12,11 @@ const forecast = ({ latitude, longitude, location }, callback) => {
       const unix_time = body.currently.time;
       const dateObject = new Date(unix_time * 1000);
       const time = dateObject.toUTCString();
+      const precipitationType =
+        body.currently.preciptationType || "precipitation";
       callback(
         undefined,
-        `Forecast overview for ${location} on ${time}: It is currently ${body.currently.temperature} degrees out. ${body.daily.data[0].summary} The high will be ${body.daily.data.temperatureHigh} with a low of ${body.daily.data.temperatureLow}. It currently feels like ${body.currently.apparentTemperature}. There is a ${body.currently.precipProbability}% chance of ${body.currently.precipType}.`
+        `Forecast overview for ${location} on ${time}: It is currently ${body.currently.temperature} degrees out. ${body.daily.data[0].summary} The high will be ${body.daily.data.temperatureHigh} with a low of ${body.daily.data.temperatureLow}. It currently feels like ${body.currently.apparentTemperature}. There is a ${body.currently.precipProbability}% chance of ${precipitationType}.`
       );
     }
   });
